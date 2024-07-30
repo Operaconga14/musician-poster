ef8749674ef148ee9c09242f0ba88bc356c71da7
Operaconga14 <operacongahub@gmail.com>
2024-07-30 06:34:21 +0100

    Front End Pages are created and Ready to start desing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultLayoutComponent } from "./components/layouts/default-layout/default-layout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DefaultLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Musician Poster';
 }
