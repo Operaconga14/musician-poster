c707f2c1c7610c94a667f757dda44d438aaf7a71
Operaconga14 <operacongahub@gmail.com>
2024-08-27 17:31:21 +0100

    Change from testing to development and code the basics of Events, Posts, User and Gigs updated

--- api/db/db_connection.js
+++ api/db/db_connection.js
@@ -1,17 +1,30 @@
 const mysql2 = require('mysql2')
+const { cloud_db_options } = require('../config/config')
 
 
 const mysql = mysql2.createConnection({
-    host: 'localhost',
-    database: 'mugivies_test',
-    user: 'root',
-    password: 'Operaconga@12',
+    host: cloud_db_options.db_host,
+    database: cloud_db_options.db_name,
+    user: cloud_db_options.db_user,
+    password: cloud_db_options.db_pass,
     connectionLimit: 5,
     connectTimeout: 600000,
     ssl: {
         rejectUnauthorized: false
     }
 })
+
+// const mysql = mysql2.createConnection({
+//     host: 'localhost',
+//     database: 'mugivies_test',
+//     user: 'root',
+//     password: 'Operaconga@12',
+//     connectionLimit: 5,
+//     connectTimeout: 600000,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// })
 
 
 // connect to local database
